e9c46bb4f9d34a5df4f1caa899849f3dd463315e
Carlos Alberto <carllos.seg@gmail.com>
2022-07-22 12:26:29 -0300

    pagina novo usuário

--- src/app/home/home-routing.module.ts
+++ src/app/home/home-routing.module.ts
@@ -2,6 +2,7 @@ import { LoginComponent } from './login/login.component';
 import { HomeComponent } from './home.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { NovoUsuarioComponent } from './novo-usuario/novo-usuario.component';
 
 const routes: Routes = [
 {
@@ -12,9 +13,13 @@ const routes: Routes = [
       {
         path: '',
         component: LoginComponent,     /* Aqui eu estou criando uma subRota dentro da rota Home, para chamar a pagina Login */
-      }
-    ]
-}
+      },
+      {
+        path: 'novousuario',
+        component: NovoUsuarioComponent,
+      },
+    ],
+},
 
 ];
 
